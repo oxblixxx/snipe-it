b8e8c53e9f72e6c9ad214f70e785e8528c68a5f6
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2024-01-03 17:09:38 +0100

    Update docker.sh

--- ansible/ubuntu/roles/packages/files/docker.sh
+++ ansible/ubuntu/roles/packages/files/docker.sh
@@ -13,3 +13,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-compose -y
