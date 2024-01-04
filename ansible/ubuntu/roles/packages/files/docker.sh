e53c847a565fbcd22c25f5d54136997bcf4ab6ff
oxblixxx <phamust1111@gmail.com>
2024-01-05 00:30:31 +0100

    refactored docker.sh file

--- ansible/ubuntu/roles/packages/files/docker.sh
+++ ansible/ubuntu/roles/packages/files/docker.sh
@@ -1,9 +1,12 @@
 #https://docs.docker.com/engine/install/ubuntu/#uninstall-old-versions
+
 # Add Docker's official GPG key:
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+if [[ ! -f /etc/apt/keyrings/docker.gpg ]]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+fi
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
@@ -12,5 +15,26 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
+
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo apt install docker-compose -y
+
+
+
+
+# # Add Docker's official GPG key:
+# sudo apt-get update -y
+# sudo apt-get install ca-certificates curl gnupg -y
+# sudo install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# # Add the repository to Apt sources:
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# sudo apt install docker-compose -y
--- journal.md
+++ journal.md
@@ -17,3 +17,34 @@ Indefinite loading of Sonarqube. This error was fixed by installing apache2.
 ```sh
 sudo apt install apache2
 ```
+---
+
+## Forgot password
+I forgot the password to my sonarqube instance, spinned up docker-compose file. I logged into the postgres container with `psql -d mydb -U myuser` then I ran this command from the [documentation](https://docs.sonarsource.com/sonarqube/latest/instance-administration/security/)
+
+```sh
+update users set
+crypted_password='100000$t2h8AtNs1AlCHuLobDjHQTn9XppwTIx88UjqUm4s8RsfTuXQHSd/fpFexAnewwPsO6jGFQUv/24DnO55hY6Xew==',
+salt='k9x9eN127/3e/hf38iNiKwVfaVk=',
+hash_method='PBKDF2',
+reset_password='true',
+user_local='true',
+active='true'
+where login='admin';
+```
+
+Refreshed browser then it works!!!.
+
+
+## UPDATE CONF SONAR.PROPERTIES
+I needed to update the configuration file, but then I don't have a text editor shipped and sudo priviledges. Then I find this helpful from [stackoverflow](https://stackoverflow.com/questions/30853247/how-do-i-edit-a-file-after-i-shell-to-a-docker-container)
+
+```sh
+docker cp <container>:/path/to/file.ext .
+```
+
+while done editing the file, do this to copy back to the running container to replace the old file
+
+```sh
+docker cp file.ext <container>:/path/to/file.ext
+```
